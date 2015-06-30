749e24a51cce0e390ba88f64c6e7891ee9af173b
Shaun P Sullivan <shaun@fisheyesoup.com>
2015-06-30 16:19:28 -0400

    data file

---
+++ data.js
@@ -0,0 +1,81 @@
+var movies = [{
+  "Title": 2001 A Space Oddessey,
+  "moviePoster ": "<img src='images/2001_poster.jpg'>",
+  "plotDescription": "This is a story of evolution. Sometime in the distant past, someone or something nudged evolution by placing a monolith on Earth (presumably elsewhere throughout the universe as well). Evolution then enabled humankind to reach the moon's surface, where yet another monolith is found, one that signals the monolith placers that humankind has evolved that far. Now a race begins between computers (HAL) and human (Bowman) to reach the monolith placers. The winner will achieve the next step in evolution, whatever that may be.",
+  "rating": pg,
+},{
+  "Title": Lawrence of Arabia,
+  "moviePoster ": "<img src='images/LawofArabia_poster.jpg'>",
+  "plotDescription": "An inordinately complex man who has been labeled everything from hero, to charlatan, to sadist, Thomas Edward Lawrence blazed his way to glory in the Arabian desert, then sought anonymity as a common soldier under an assumed name. The story opens with the death of Lawrence in a motorcycle accident in Dorset at the age of 46, then flashbacks to recount his adventures: as a young intelligence officer in Cairo in 1916, he is given leave to investigate the progress of the Arab revolt against the Turks in World War I. In the desert, he organizes a guerrilla army and--for two years--leads the Arabs in harassing the Turks with desert raids, train-wrecking and camel attacks. Eventually, he leads his army northward and helps a British General destroy the power of the Ottoman Empire.",
+  "rating": g,
+}, {
+  "Title": 2001 A Space Oddessey,
+  "moviePoster ": "<img src='images/2001_poster.jpg'>",
+  "plotDescription": "This is a story of evolution. Sometime in the distant past, someone or something nudged evolution by placing a monolith on Earth (presumably elsewhere throughout the universe as well). Evolution then enabled humankind to reach the moon's surface, where yet another monolith is found, one that signals the monolith placers that humankind has evolved that far. Now a race begins between computers (HAL) and human (Bowman) to reach the monolith placers. The winner will achieve the next step in evolution, whatever that may be.",
+  "rating": pg,
+}, {
+  "Title": Lawrence of Arabia,
+  "moviePoster ": "<img src='images/LawofArabia_poster.jpg'>",
+  "plotDescription": "An inordinately complex man who has been labeled everything from hero, to charlatan, to sadist, Thomas Edward Lawrence blazed his way to glory in the Arabian desert, then sought anonymity as a common soldier under an assumed name. The story opens with the death of Lawrence in a motorcycle accident in Dorset at the age of 46, then flashbacks to recount his adventures: as a young intelligence officer in Cairo in 1916, he is given leave to investigate the progress of the Arab revolt against the Turks in World War I. In the desert, he organizes a guerrilla army and--for two years--leads the Arabs in harassing the Turks with desert raids, train-wrecking and camel attacks. Eventually, he leads his army northward and helps a British General destroy the power of the Ottoman Empire.",
+  "rating": g,
+}, {
+  "Title": 2001 A Space Oddessey,
+  "moviePoster ": "<img src='images/2001_poster.jpg'>",
+  "plotDescription": "This is a story of evolution. Sometime in the distant past, someone or something nudged evolution by placing a monolith on Earth (presumably elsewhere throughout the universe as well). Evolution then enabled humankind to reach the moon's surface, where yet another monolith is found, one that signals the monolith placers that humankind has evolved that far. Now a race begins between computers (HAL) and human (Bowman) to reach the monolith placers. The winner will achieve the next step in evolution, whatever that may be.",
+  "rating": pg,
+}, {
+  "Title": Lawrence of Arabia,
+  "moviePoster ": "<img src='images/LawofArabia_poster.jpg'>",
+  "plotDescription": "An inordinately complex man who has been labeled everything from hero, to charlatan, to sadist, Thomas Edward Lawrence blazed his way to glory in the Arabian desert, then sought anonymity as a common soldier under an assumed name. The story opens with the death of Lawrence in a motorcycle accident in Dorset at the age of 46, then flashbacks to recount his adventures: as a young intelligence officer in Cairo in 1916, he is given leave to investigate the progress of the Arab revolt against the Turks in World War I. In the desert, he organizes a guerrilla army and--for two years--leads the Arabs in harassing the Turks with desert raids, train-wrecking and camel attacks. Eventually, he leads his army northward and helps a British General destroy the power of the Ottoman Empire.",
+  "rating": g,
+}, {
+  "Title": 2001 A Space Oddessey,
+  "moviePoster ": "<img src='images/2001_poster.jpg'>",
+  "plotDescription": "This is a story of evolution. Sometime in the distant past, someone or something nudged evolution by placing a monolith on Earth (presumably elsewhere throughout the universe as well). Evolution then enabled humankind to reach the moon's surface, where yet another monolith is found, one that signals the monolith placers that humankind has evolved that far. Now a race begins between computers (HAL) and human (Bowman) to reach the monolith placers. The winner will achieve the next step in evolution, whatever that may be.",
+  "rating": pg,
+}, {
+  "Title": Lawrence of Arabia,
+  "moviePoster ": "<img src='images/LawofArabia_poster.jpg'>",
+  "plotDescription": "An inordinately complex man who has been labeled everything from hero, to charlatan, to sadist, Thomas Edward Lawrence blazed his way to glory in the Arabian desert, then sought anonymity as a common soldier under an assumed name. The story opens with the death of Lawrence in a motorcycle accident in Dorset at the age of 46, then flashbacks to recount his adventures: as a young intelligence officer in Cairo in 1916, he is given leave to investigate the progress of the Arab revolt against the Turks in World War I. In the desert, he organizes a guerrilla army and--for two years--leads the Arabs in harassing the Turks with desert raids, train-wrecking and camel attacks. Eventually, he leads his army northward and helps a British General destroy the power of the Ottoman Empire.",
+  "rating": g,
+}, {
+  "Title": 2001 A Space Oddessey,
+  "moviePoster ": "<img src='images/2001_poster.jpg'>",
+  "plotDescription": "This is a story of evolution. Sometime in the distant past, someone or something nudged evolution by placing a monolith on Earth (presumably elsewhere throughout the universe as well). Evolution then enabled humankind to reach the moon's surface, where yet another monolith is found, one that signals the monolith placers that humankind has evolved that far. Now a race begins between computers (HAL) and human (Bowman) to reach the monolith placers. The winner will achieve the next step in evolution, whatever that may be.",
+  "rating": pg,
+},{
+  "Title": Lawrence of Arabia,
+  "moviePoster ": "<img src='images/LawofArabia_poster.jpg'>",
+  "plotDescription": "An inordinately complex man who has been labeled everything from hero, to charlatan, to sadist, Thomas Edward Lawrence blazed his way to glory in the Arabian desert, then sought anonymity as a common soldier under an assumed name. The story opens with the death of Lawrence in a motorcycle accident in Dorset at the age of 46, then flashbacks to recount his adventures: as a young intelligence officer in Cairo in 1916, he is given leave to investigate the progress of the Arab revolt against the Turks in World War I. In the desert, he organizes a guerrilla army and--for two years--leads the Arabs in harassing the Turks with desert raids, train-wrecking and camel attacks. Eventually, he leads his army northward and helps a British General destroy the power of the Ottoman Empire.",
+  "rating": g,
+}, {
+  "Title": 2001 A Space Oddessey,
+  "moviePoster ": "<img src='images/2001_poster.jpg'>",
+  "plotDescription": "This is a story of evolution. Sometime in the distant past, someone or something nudged evolution by placing a monolith on Earth (presumably elsewhere throughout the universe as well). Evolution then enabled humankind to reach the moon's surface, where yet another monolith is found, one that signals the monolith placers that humankind has evolved that far. Now a race begins between computers (HAL) and human (Bowman) to reach the monolith placers. The winner will achieve the next step in evolution, whatever that may be.",
+  "rating": pg,
+}, {
+  "Title": Lawrence of Arabia,
+  "moviePoster ": "<img src='images/LawofArabia_poster.jpg'>",
+  "plotDescription": "An inordinately complex man who has been labeled everything from hero, to charlatan, to sadist, Thomas Edward Lawrence blazed his way to glory in the Arabian desert, then sought anonymity as a common soldier under an assumed name. The story opens with the death of Lawrence in a motorcycle accident in Dorset at the age of 46, then flashbacks to recount his adventures: as a young intelligence officer in Cairo in 1916, he is given leave to investigate the progress of the Arab revolt against the Turks in World War I. In the desert, he organizes a guerrilla army and--for two years--leads the Arabs in harassing the Turks with desert raids, train-wrecking and camel attacks. Eventually, he leads his army northward and helps a British General destroy the power of the Ottoman Empire.",
+  "rating": g,
+}, {
+  "Title": 2001 A Space Oddessey,
+  "moviePoster ": "<img src='images/2001_poster.jpg'>",
+  "plotDescription": "This is a story of evolution. Sometime in the distant past, someone or something nudged evolution by placing a monolith on Earth (presumably elsewhere throughout the universe as well). Evolution then enabled humankind to reach the moon's surface, where yet another monolith is found, one that signals the monolith placers that humankind has evolved that far. Now a race begins between computers (HAL) and human (Bowman) to reach the monolith placers. The winner will achieve the next step in evolution, whatever that may be.",
+  "rating": pg,
+}, {
+  "Title": Lawrence of Arabia,
+  "moviePoster ": "<img src='images/LawofArabia_poster.jpg'>",
+  "plotDescription": "An inordinately complex man who has been labeled everything from hero, to charlatan, to sadist, Thomas Edward Lawrence blazed his way to glory in the Arabian desert, then sought anonymity as a common soldier under an assumed name. The story opens with the death of Lawrence in a motorcycle accident in Dorset at the age of 46, then flashbacks to recount his adventures: as a young intelligence officer in Cairo in 1916, he is given leave to investigate the progress of the Arab revolt against the Turks in World War I. In the desert, he organizes a guerrilla army and--for two years--leads the Arabs in harassing the Turks with desert raids, train-wrecking and camel attacks. Eventually, he leads his army northward and helps a British General destroy the power of the Ottoman Empire.",
+  "rating": g,
+}, {
+  "Title": 2001 A Space Oddessey,
+  "moviePoster ": "<img src='images/2001_poster.jpg'>",
+  "plotDescription": "This is a story of evolution. Sometime in the distant past, someone or something nudged evolution by placing a monolith on Earth (presumably elsewhere throughout the universe as well). Evolution then enabled humankind to reach the moon's surface, where yet another monolith is found, one that signals the monolith placers that humankind has evolved that far. Now a race begins between computers (HAL) and human (Bowman) to reach the monolith placers. The winner will achieve the next step in evolution, whatever that may be.",
+  "rating": pg,
+}, {
+  "Title": Lawrence of Arabia,
+  "moviePoster ": "<img src='images/LawofArabia_poster.jpg'>",
+  "plotDescription": "An inordinately complex man who has been labeled everything from hero, to charlatan, to sadist, Thomas Edward Lawrence blazed his way to glory in the Arabian desert, then sought anonymity as a common soldier under an assumed name. The story opens with the death of Lawrence in a motorcycle accident in Dorset at the age of 46, then flashbacks to recount his adventures: as a young intelligence officer in Cairo in 1916, he is given leave to investigate the progress of the Arab revolt against the Turks in World War I. In the desert, he organizes a guerrilla army and--for two years--leads the Arabs in harassing the Turks with desert raids, train-wrecking and camel attacks. Eventually, he leads his army northward and helps a British General destroy the power of the Ottoman Empire.",
+  "rating": g,
+}];
